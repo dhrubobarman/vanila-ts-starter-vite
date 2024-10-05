88d072e5ad761d564d651990174610e992e40d9e
Dhrubojyoti Barman <47059504+dhrubobarman@users.noreply.github.com>
2024-10-05 15:02:37 +0530

    Update createElement.ts

--- src/utils/createElement.ts
+++ src/utils/createElement.ts
@@ -1,16 +1,19 @@
 export function createElement<T extends keyof HTMLElementTagNameMap>(
   tag: T,
   attributes?: Partial<HTMLElementTagNameMap[T]>,
-  insertToApp: string | boolean = true
+  insertToApp: string | boolean | HTMLElement = true
 ): HTMLElementTagNameMap[T] {
-  const element = document.createElement(tag);
+  let element = document.createElement(tag);
   if (attributes) {
     Object.assign(element, attributes);
   }
-  if (typeof insertToApp === "string") {
+  if (typeof insertToApp === 'string') {
     document.querySelector(insertToApp)?.appendChild(element);
-  } else if (insertToApp) {
-    document.querySelector("#app")?.appendChild(element);
+  } else if (insertToApp === true) {
+    document.querySelector('#app')?.appendChild(element);
+  } else if (typeof insertToApp === 'object') {
+    const container = insertToApp as HTMLElement;
+    container.appendChild?.(element);
   }
   return element;
 }
